f7e4b56bb575130d5b7fb890e2674da04ec801ca
Steven Schronk <steven@schronk.com>
2009-12-30 11:21:39 -0600

    Basic functionality of Error and Warning messages. Added two functions that allow the assembler to warn users of pooly written code as well as the ability to halt when something is not right.
    Also added an ERRORS file where some documentation will be kept about all errors
    that are supported in the code. This will help provide some support for the
    user community.

--- asm.c
+++ asm.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+void exit_error(const int err_num, const char *err_msg)
 {
+	fprintf(stderr, "ERROR %d: %s\n", err_num, err_msg);
+	// TODO: close file handles here
+	exit(EXIT_FAILURE);
+}
 
+int warning_error(const int err_num, const char *err_msg)
+{
+	fprintf(stderr, "WARNING: %d: %s\n", err_num, err_msg);
+}
 
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	if(argc != 2) { exit_error('1', "No Input Files"); }
+	// TODO: future versions will accept more than one file
+	if(argc > 2) {exit_error('2', "Too Many Files Listed"); }
 
 	return 0;
 }
+
---
+++ asm.h
@@ -0,0 +1,21 @@
+#include "asm.c"
+
+/*
+* Display Errors to user as needed.
+*
+* Will send text of error message as well as error number.
+* Error number corresponds to number listed in documentation.
+*
+*/
+void exit_error(const int err_num, const char *err_msg);
+
+/*
+* Display Warnings about assembler status as needed.
+*
+* These are messages to the user that contain both an error number
+* as well as simple description of what went wrong.
+* 
+* Error number corresponds to number listed in documentation.
+*
+*/
+int warning_error(const int err_num, const char *err_msg)
