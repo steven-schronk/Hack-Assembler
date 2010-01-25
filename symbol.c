d6ec9a3f025c178e5d3f123c722267e08147d744
Steven Schronk <steven@schronk.com>
2010-01-24 23:31:02 -0600

    Symbol table now supports both name and address of symbol.
    This was added to track the actual address that each symbol
    will be relaced with during code generation.

--- symbol.c
+++ symbol.c
@@ -6,16 +6,17 @@
 #include "parse.h"
 #include "symbol.h"
 
-char *symbol_hash[HASH_SIZE];
+struct symbol_hash symbol_hash[HASH_SIZE];
+int rom_address = 0;
 
 void print_hash()
 {
 	int i = 0;
 	while(i < HASH_SIZE)
 	{
-		if(symbol_hash[i] != NULL)
+		if(symbol_hash[i].name != NULL)
 		{
-			printf("HASH: %d->%s\n", i, symbol_hash[i]);
+			printf("HASH: %d->%d->%s\n", i, symbol_hash[i].address, symbol_hash[i].name);
 		}
 		++i;
 	}
@@ -39,19 +40,21 @@ int add_entry(char symbol[])
 	int i = 0;
 	int hash_val;
 	hash_val = hash(symbol);
-	if(symbol_hash[i] == NULL)
+	if(symbol_hash[i].name == NULL)
 	{
-		symbol_hash[hash_val] = symbol;
+		symbol_hash[hash_val].name = symbol;
+		symbol_hash[hash_val].address = rom_address++;
 		return 1;
 	} else {
-		do{ ++i; } while (symbol_hash[i] != NULL && i < HASH_SIZE);
+		do{ ++i; } while (symbol_hash[i].name != NULL && i < HASH_SIZE);
 		if( i == HASH_SIZE)
 		{
 			int i = find_line_num();
 			line_notification(i);
 			exit_error(13, "Symbol table full");
 		}
-		symbol_hash[hash_val] = symbol;
+		symbol_hash[hash_val].name = symbol;
+		symbol_hash[hash_val].address = rom_address++;
 		return 1;
 	}
 	return 0;
@@ -61,7 +64,7 @@ int get_address(char symbol[])
 {
 	int hash_val, i = 0, j = 0;
 	hash_val = hash(symbol);
-	if(symbol_hash[hash_val] != NULL)
+	if(symbol_hash[hash_val].name != NULL)
 	{
 		return hash_val;
 	} else {
@@ -69,12 +72,12 @@ int get_address(char symbol[])
 		could not find hash -- loop through strings
 		for length of symbol_hash looking for a match
 		*/
-		while(strcmp(symbol_hash[i],symbol) != 0 && j < HASH_SIZE)
+		while(strcmp(symbol_hash[i].name, symbol) != 0 && j < HASH_SIZE)
 		{
 			if(i == HASH_SIZE){ i = 0; } else { ++i; }
 			++j;
 		}
-		if(strcmp(symbol_hash[i],symbol) == 0)
+		if(strcmp(symbol_hash[i].name, symbol) == 0)
 		{
 			return i;
 		}
--- symbol.h
+++ symbol.h
@@ -1,5 +1,11 @@
 #define HASH_SIZE 509
 
+struct symbol_hash
+{
+	char *name;
+	int address;
+};
+
 /*
 * Prints out hash.
 * Must pass hash pointer and length of hash array
@@ -7,24 +13,19 @@
 void print_hash();
 
 /*
-* Hash function.
+* Calculates Hash value.
 * Returns integer value of hash based on string input
 */
 int hash(char symbol[]);
 
 /*
-* Adds pair (symbol, address) to the table
+* Adds pair (symbol, address) to the table.
 * Returns 1 if entry added to table and 0 otherwise.
 */
 int add_entry(char symbol[]);
 
 /*
-* Does the symbol table contain the given symbol
-*/
-int contains(char symbol[]);
-
-/*
-* Returns address associated with symbol
+* Returns address associated with symbol.
 * If array found in hash, address is returned,
 * otherwise return -1
 */
